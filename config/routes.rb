16b30c7e31c55b47c45d8d4adccb50b6640d4863
Niyi Adewole <9janeo@gmail.com>
2018-05-08 20:09:59 -0500

    Updated rails to 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+	root 'keywords#index'
+  
+  resources :consultants
+  resources :services
+  resources :keywords
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
