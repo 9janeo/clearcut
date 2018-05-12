00d1063bf8b6d004329f835ec505094c48c0ea67
Niyi Adewole <9janeo@gmail.com>
2018-05-11 23:09:39 -0500

    Added estimates model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+
 	root 'keywords#index'
   
 	  resources :consultants
 	  resources :services
 	  resources :keywords
+	  resources :estimates
+
+  get 'estimates', to: 'estimates#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
