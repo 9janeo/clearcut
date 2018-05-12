9cc1cee0f2940b01c3700b80ef3bb34612937634
Niyi Adewole <9janeo@gmail.com>
2018-05-11 23:12:54 -0500

    Estimates (#11)
    * service index condition
    
    * consultant index condition
    
    * Added estimates model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320195129) do
+ActiveRecord::Schema.define(version: 20180509045902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20180320195129) do
     t.string "twitter_handle"
     t.string "facebook_url"
     t.string "instagram_handle"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "estimates", force: :cascade do |t|
+    t.string "type"
+    t.datetime "start"
+    t.datetime "end"
+    t.string "owner"
+    t.string "email"
+    t.string "phone"
+    t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
