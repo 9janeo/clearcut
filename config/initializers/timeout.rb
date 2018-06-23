561f2d28b83b647cd5f65b7a33504a485ae42789
Niyi Adewole <9janeo@gmail.com>
2018-06-23 04:38:10 -0500

    Changed timeout initializer config to account for legacy class setter (#17)

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,4 @@
 # config/initializers/timeout.rb
-Rack::Timeout.service_timeout = 10  # seconds
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 20
+
+# Rack::Timeout.service_timeout = 10  # seconds
